8e9bdfd8a4ccb3e8580e6ce8d0829acda5d7c67d
Bruno <bruno_bury@hotmail.com>
2023-02-21 15:47:00 -0300

    exercicio-01-finalizado

--- fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-01scripts.js
+++ fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-01scripts.js
@@ -1,4 +1,7 @@
-let age = 20;
-let name = "hulk";
-console.log(name);
-console.log(age);
+let myName = "Bruno";
+let birtthCity = "Curitiba";
+let birthYear = 1990;
+
+console.log(myName);
+console.log(birtthCity);
+console.log(birthYear);
