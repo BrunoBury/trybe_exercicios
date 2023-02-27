58900f6298357ccc05bbded9fa815cc490552f39
Bruno <bruno_bury@hotmail.com>
2023-02-27 20:42:59 -0300

    exercicio-07dia-01, parte 2

---
+++ fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-08scripts.js
@@ -0,0 +1,18 @@
+//Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
+let a = 19;
+let b = 18;
+let c = 13;
+if (a>b) {
+    console.log('a é maior que b')
+    
+}else{
+    console.log('b é maior que a');
+}
+//Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+if (a>b && a>c){
+    console.log('a é o maior número;');
+}else if(b>a && b>c){
+    console.log('b é o maior número');
+}else if(c>a && c>b){
+    console.log('c é o maior número');
+}
