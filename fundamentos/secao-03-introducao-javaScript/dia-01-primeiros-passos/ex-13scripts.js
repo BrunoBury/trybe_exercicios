1aae55f63f519dba5f5dfde0884e117d53caa33f
Bruno <bruno_bury@hotmail.com>
2023-02-27 23:04:28 -0300

    exercicio impar true

--- fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-13scripts.js
+++ fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-13scripts.js
@@ -1,10 +1,20 @@
 //Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, o código deve retornar false.Bônus: use somente um if.
-let a = 49
-let b = 63
-let c = 71
+let a = 49;
+let b = 62;
+let c = 71;
 if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
-    console.log(true)
+    console.log(true);
 }
 else{
-    console.log(false)
+    console.log(false);
+}
+//Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, o código deve retornar false.Bônus: use somente um if.
+ let a1 = 48;
+ let b2 = 63;
+ let c3 = 70;
+ if (a1% 2 !== 0 || b2 % 2 !== 0 || c3% 2 !== 0) {
+    console.log(true);
+}
+else{
+    console.log(false);
 }
