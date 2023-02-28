e547e881f5a49027c23b7acc7500cc58b836a6ae
Bruno <bruno_bury@hotmail.com>
2023-02-28 12:22:51 -0300

    adicionado aliquota IR

--- fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-15scripst.js
+++ fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-15scripst.js
@@ -23,8 +23,8 @@
 
 let aliquotaInss;
 let aliquotaIr;
-let salarioBruto = 2500;
-let salarioBase = salarioBruto - aliquotaInss;
+let salarioBruto = 3500;
+
 
 if (salarioBruto <= 1556.94 ) {
     aliquotaInss = salarioBruto * 0.08;    
@@ -38,13 +38,25 @@ else if(salarioBruto >=2594.93 && salarioBruto <= 5189.82){
 else{
     aliquotaInss = 570.88;
 }
+let salarioBase = salarioBruto - aliquotaInss;
 
-let salarioliquido = salarioBase - aliquotaIr;
 
 if(salarioBase <= 1903.98){
     aliquotaIr = 0;
 }
 else if (salarioBase >= 1903.99 && salarioBase <= 2826.65){
-    aliquotaIr = (salarioBase * 0.075) - 142.80
+    aliquotaIr = (salarioBase * 0.075)-142.80;
 }
-console.log(salarioliquido);
+else if(salarioBase >= 2826.66 && salarioBase <= 3751.05){
+    aliquotaIr = (salarioBase * 0.15)-354.80;
+}
+else if (salarioBase >= 3751.06 && salarioBase <= 4664.68){
+    aliquotaIr = (salarioBase * 0.225)-636.13; 
+}
+else  {
+    aliquotaIr = (salarioBase * 0.275)-869.36;
+}
+
+
+let salarioLiquido = salarioBase - aliquotaIr;
+console.log(salarioLiquido);
