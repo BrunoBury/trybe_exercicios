5fc3f606e7dd5e83386a407759be06b80b8de2a9
Bruno <bruno_bury@hotmail.com>
2023-02-28 10:39:46 -0300

    exercicio venda produto lucro e imposto

--- fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-10scripts.js
+++ fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-10scripts.js
@@ -4,9 +4,9 @@ let angulo1 = 60;
 let angulo2 = -10;
 let angulo3 = 60;
 
-if(angulo1+angulo2+angulo3===180){
+if(angulo1 + angulo2 + angulo3 === 180){
     console.log(true)
-} else if(angulo1<0 || angulo2<0 || angulo3<0){
+} else if(angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
     console.log('Ângulo invalido!')    
 } else{
     console.log(false)
