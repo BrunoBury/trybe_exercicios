f183504b0fad6da7405e58570c799f87ba9171c6
Bruno <bruno_bury@hotmail.com>
2023-02-27 23:00:27 -0300

    exercicio numero par true

---
+++ fundamentos/secao-03-introducao-javaScript/dia-01-primeiros-passos/ex-13scripts.js
@@ -0,0 +1,10 @@
+//Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, o código deve retornar false.Bônus: use somente um if.
+let a = 49
+let b = 63
+let c = 71
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+    console.log(true)
+}
+else{
+    console.log(false)
+}
