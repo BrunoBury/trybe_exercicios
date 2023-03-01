90c9e385f75d381e657aaccfe0d28fb5f94035c6
Bruno <bruno_bury@hotmail.com>
2023-03-01 00:01:55 -0300

    contando numeros ímpar

---
+++ fundamentos/secao-03-introducao-javaScript/dia-02-array-e-loop-for/ex-04scripts.js
@@ -0,0 +1,13 @@
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let oddNumber = 0;
+for(let index = 0; index < numbers.length; index += 1){
+  if(numbers[index] % 2 !== 0 ){
+    oddNumber +=1;
+  }
+}
+console.log(oddNumber)
+if (oddNumber ===0){
+    console.log('Nenhum valor ímpar encontrado');
+}
