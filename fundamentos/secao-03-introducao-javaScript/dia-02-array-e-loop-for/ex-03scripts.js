97f57467118bc2c7da439245419e4f48456cc788
Bruno <bruno_bury@hotmail.com>
2023-02-28 22:57:45 -0300

    exercicio-03 percorrendo array

---
+++ fundamentos/secao-03-introducao-javaScript/dia-02-array-e-loop-for/ex-03scripts.js
@@ -0,0 +1,7 @@
+//🚀 Lidando com arrays
+//Considere esse array para realizar os próximos exercícios.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);    
+}
