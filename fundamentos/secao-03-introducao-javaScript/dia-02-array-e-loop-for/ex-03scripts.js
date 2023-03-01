5a396d9dbed7dbaafb37f60e586ad7e5fb400def
Bruno <bruno_bury@hotmail.com>
2023-02-28 23:22:16 -0300

    valor maior ou menor que 20

--- fundamentos/secao-03-introducao-javaScript/dia-02-array-e-loop-for/ex-03scripts.js
+++ fundamentos/secao-03-introducao-javaScript/dia-02-array-e-loop-for/ex-03scripts.js
@@ -14,4 +14,10 @@ console.log(sum);
 //Calcule e imprima a média aritmética dos valores contidos no array.
 let average = sum/numbers.length;
 console.log(average);
-//
+//Com base no código que acabou de gerar, faça com que: caso o valor final seja maior que 20, imprima a mensagem “Valor maior que 20”; e, caso não seja maior que 20, imprima a mensagem “Valor menor ou igual a 20”.
+if (average > 20){
+  console.log('Valor maior que 20');
+}
+else{
+  console.log('Valor menor ou igual a 20');
+}
