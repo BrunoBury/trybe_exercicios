bf5c5457e5e8d2217adbcd1a9bca001831bc94f3
Bruno <bruno_bury@hotmail.com>
2023-03-05 18:58:08 -0300

    exercicio pedido clientes

---
+++ fundamentos/secao-03-introducao-javaScript/dia-04-objetos/ex-07scripts.js
@@ -0,0 +1,72 @@
+// Complete a função customerInfo() para que seu retorno seja similar a 'Olá, Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701'.
+//Complete a função orderModifier() para que seu retorno seja similar a 'Olá, Luiz Silva, o valor total de seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00.';
+// Modifique o nome da pessoa compradora para Luiz Silva;
+// Modifique o valor total da compra para R$ 50,00.
+
+let order = {
+  name: "Rafael Andrade",
+  phoneNumber: "11-98763-1416",
+  address: {
+    street: "Rua das Flores",
+    number: "389",
+    apartment: "701",
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: "Coca-Cola Zero",
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: "Ana Silveira",
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+//   console.log(order)
+
+function customerInfo(order) {
+  console.log(
+    "Olá " +
+      order.order.delivery.deliveryPerson +
+      ", entrega para: " +
+      order.name +
+      ", telefone:" +
+      order.phoneNumber +
+      ", R. " +
+      order.address.street +
+      ", Nº: " +
+      order.address.number +
+      " AP: " +
+      order.address.apartment
+  );
+}
+
+customerInfo(order);
+
+function orderModifier(order) {
+  order.name = "Luiz Silva";
+  order.payment.total = "50";
+  let pizzas = Object.keys(order.order.pizza);
+
+  console.log(
+    "Olá " + order.name + ", o total do seu pedido de " + pizzas[0] + ", " + pizzas[1] + " e " + order.order.drinks.coke.type + " é R$ " + order.payment.total +",00."
+  );
+}
+
+orderModifier(order);
