53bdf7201c86a5ddfff0bdefceb374223fd8e78b
Bruno <bruno_bury@hotmail.com>
2023-03-06 12:13:16 -0300

    verifica par de chave/valor

--- fundamentos/secao-03-introducao-javaScript/dia-04-objetos/ex-08scripts.js
+++ fundamentos/secao-03-introducao-javaScript/dia-04-objetos/ex-08scripts.js
@@ -5,59 +5,71 @@
 // Crie um objeto de nome allLessons, que deve agrupar todas as aulas por meio do Object.assign. Cada chave desse novo objeto será uma aula, portanto essas chaves serão nomeadas lesson1, lesson2 e lesson3. Ao executar o comando
 // Com base no objeto elaborado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
 // Crie uma função que obtenha o valor da chave de acordo com sua posição no objeto.
+//Crie uma função que verifique se o par chave/valor existe na função. Essa função deve possuir três parâmetros:o objeto, o nome da chave e o valor dela. 
 let lesson1 = {
-    materia: 'Matemática',
-    numeroEstudantes: 20,
-    professor: 'Maria Clara',
-    turno: 'manhã',
-  };
-  
-  let lesson2 = {
-    materia: 'História',
-    numeroEstudantes: 20,
-    professor: 'Carlos',
-  };
-  
-  let lesson3 = {
-    materia: 'Matemática',
-    numeroEstudantes: 10,
-    professor: 'Maria Clara',
-    turno: 'noite',
-  };
-  function addProperty(object,key,value){
+  materia: "Matemática",
+  numeroEstudantes: 20,
+  professor: "Maria Clara",
+  turno: "manhã",
+};
+
+let lesson2 = {
+  materia: "História",
+  numeroEstudantes: 20,
+  professor: "Carlos",
+};
+
+let lesson3 = {
+  materia: "Matemática",
+  numeroEstudantes: 10,
+  professor: "Maria Clara",
+  turno: "noite",
+};
+function addProperty(object, key, value) {
   object[key] = value;
-  }
-  addProperty(lesson2, 'turno', 'noite');
+}
+addProperty(lesson2, "turno", "noite");
 // console.log(lesson2);
 
-function listKeys(object){
-   return  Object.keys(object)
+function listKeys(object) {
+  return Object.keys(object);
 }
 // console.log(listKeys(lesson1))
 
-function sizeOfObject(object){
-    return Object.keys(object).length
+function sizeOfObject(object) {
+  return Object.keys(object).length;
 }
 // console.log(sizeOfObject(lesson1))
-function objValue(object){
-    return Object.values(object)
+function objValue(object) {
+  return Object.values(object);
 }
 // console.log(objValue(lesson1))
 
-const allLessons = Object.assign({},{lesson1, lesson2, lesson3})
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 // console.log(Object.keys(allLessons).length)
 
-function allStudents(object){
-    const keys = Object.keys(object)
-    let sum = 0;
-    for(let index = 0; index < keys.length ;index +=1){
-        sum = object[keys[index]].numeroEstudantes + sum;         
-    }
-    // console.log(sum)
+function allStudents(object) {
+  const keys = Object.keys(object);
+  let sum = 0;
+  for (let index = 0; index < keys.length; index += 1) {
+    sum = object[keys[index]].numeroEstudantes + sum;
+  }
+  // console.log(sum)
 }
-    // allStudents(allLessons)
-    
-    function valueNumber(object,position){
-      return Object.values(object)[position]
-        }
-    console.log(valueNumber(lesson1,0))
+// allStudents(allLessons)
+
+function valueNumber(object, position) {
+  return Object.values(object)[position];
+}
+// console.log(valueNumber(lesson1, 0));
+
+function verifyPair (object, key, value){
+if(object && key && object[key] === value){
+  return true
+} else { 
+  return false
+}
+}  
+console.log(verifyPair(lesson3, 'turno', 'noite'))
+
+
