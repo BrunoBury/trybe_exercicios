5800eae8a36b1229cb66881ed5645f15376beb52
Bruno <bruno_bury@hotmail.com>
2023-03-10 21:12:24 -0300

    o

--- fundamentos/secao-03-introducao-javaScript/dia-04-objetos/ex-09scripts.js
+++ fundamentos/secao-03-introducao-javaScript/dia-04-objetos/ex-09scripts.js
@@ -2,5 +2,22 @@
 // Atenção! Esse exercício já apareceu no processo seletivo de uma grande multinacional!
 // Dicas:
 // Uma string é um array de caracteres, então cada elemento do array é uma letra.
-// // O valor de cada numeral romano é:
-function romanToDecimal(roman)
+function romanToDecimal(numRoman)
+let romanNumbers = {
+    'I': 1,
+  
+    'V': 5,
+  
+    'X': 10,
+  
+    'L': 50,
+  
+    'C': 100,
+  
+    'D': 500,
+  
+    'M': 1000,
+  
+  };
+let result = 0;
+for (let index = 0; index < numRoman.length; index +=1) {}
